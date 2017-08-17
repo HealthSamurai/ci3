922312ea4b151b6aa574100d6a61a65a0b46ede8
Aitem <muradbei@gmail.com>
2017-08-17 16:21:04 +0300

    Small UI _

--- src/ci3/server/core.clj
+++ src/ci3/server/core.clj
@@ -8,11 +8,30 @@
    [unifn.formats]
    [unifn.core :as u]
    [unifn.env :as e]
+   [hiccup.core :as hiccup]
+   [hiccup.page :as page]
+   [garden.core :as css]
+   [clojure.walk :as walk]
    [ci3.k8s :as k8s]))
+
+(defn layout [cnt]
+  (page/html5
+   [:head
+    [:meta {:charset "utf-8"}]
+    [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"}]
+    [:link {:rel "stylesheet" :href     "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"}]]
+   [:body
+    [:style (css/css [:body {:padding "0 20px"} [:.top {:padding "5px 10px" :background-color "#eee" :margin-bottom "10px"}]])]
+    [:div.container-fluid.top "ci3"]
+    cnt]))
 
 (defmethod u/*fn ::routes
   [{{routes :routes} :cache}]
   {:response {:body (var-get routes)}})
+
+(defmethod u/*fn ::index
+  [arg]
+  {:response {:body (layout [:b "CI3"])}})
 
 (defmethod u/*fn ::watches [_]
   {:response {:status 200
@@ -28,8 +47,41 @@
   [{req :request}]
   {:response {:status 200 :body "Ok! Wait for hook"}})
 
+
+(defmethod u/*fn
+  ::builds
+  [{req :request}]
+  (let [bs (walk/keywordize-keys (k8s/list k8s/cfg :builds))]
+    {:response
+     {:status 200
+      :body
+      (layout
+         [:div.container
+          (for [b (reverse (sort-by (fn [x] (get-in x [:payload :commit :timestamp])) (:items bs)))]
+              [:div {:key (get-in b [:metadata :name])}
+               [:a {   :href (str "/builds/" (get-in b [:metadata :name]))}
+                [:b (get-in b [:payload :commit :timestamp])]
+                " by "
+                [:span (get-in b [:commit :author :name])]
+                " - "
+                [:span (get-in b [:commit :message])]]
+               [:div "pod: "   (get-in b [:metadata :name])]
+               [:hr]])])
+      :headers {"Content-Type" "text/html"}}}))
+
+(defmethod u/*fn
+  ::logs
+  [{{{id :id} :route-params} :request}]
+  (let [logs (k8s/curl {} (str  "api/v1/namespaces/default/pods/build-" id "/log"))]
+    {:response
+     {:status 200
+      :body (layout
+             [:div.container-fluid
+              [:pre logs]])
+      :headers {"Content-Type" "text/html"}}}))
+
 (def routes
-  {:GET ::routes
+  {:GET ::index
    "repositories" {:GET ::repositories}
    "watches"      {:GET ::watches}
    "webhook" { [:id] {:POST ::repo/webhook
