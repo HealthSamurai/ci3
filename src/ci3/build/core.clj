b03e0ae447bf8598238aaed6ecf7709fcf0dd135
Aitem <muradbei@gmail.com>
2017-10-11 16:10:33 +0300

    Test new notifications

--- src/ci3/build/core.clj
+++ src/ci3/build/core.clj
@@ -45,7 +45,6 @@
   ::build
   [{env :env cfg :k8s {{{nm :name} :metadata :as build}  :object tp :type } :resource}]
   (let [cfg {:prefix "api" :apiVersion "v1" :ns "default"}]
-    (log/warn tp (:status build))
     (when (and (= tp "ADDED") (= "pending" (:status build)))
       (log/info "Create build pod" (str "build-" nm))
       (let [pod (k8s/create cfg :pods
