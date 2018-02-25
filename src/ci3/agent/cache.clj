ab22e4877fc7f0e5895cba2edd0c18fd27143595
Aitem <muradbei@gmail.com>
2018-02-25 15:21:40 +0300

    make minio lazy

--- src/ci3/agent/cache.clj
+++ src/ci3/agent/cache.clj
@@ -2,7 +2,7 @@
   (:import io.minio.MinioClient)
   (:require [ci3.agent.shelk :as shelk]))
 
-(def client
+(defn client []
   (MinioClient. (System/getenv "CI3_SECRET_STORAGE_HOST")
                 (System/getenv "CI3_SECRET_STORAGE_ACCESSKEY")
                 (System/getenv "CI3_SECRET_STORAGE_SECRETKEY") true ))
@@ -10,18 +10,17 @@
 (def bucket
   (or (System/getenv "CI3_CONFIG_CACHE_BUCKET") "ci-cache"))
 
-
 ;; TODO: safe read and upload
 (defn archive-dir [dir to]
   (shelk/bash ["tar" "czvf" to dir ">/dev/null 2>&1"]))
 
 (defn upload-to-bucket [file-path k]
-  (.putObject client bucket k file-path))
+  (.putObject (client) bucket k file-path))
 
 (defn download-from-bucket [file-path k]
-  (.getObject client bucket k file-path))
+  (.getObject (client) bucket k file-path))
 
 (comment
-  (.bucketExists client bucket)
+  (.bucketExists (client) bucket)
 
   )
